3c2397702a28e987472868fba6555e0fe2547046
DandinPower <tomhot246@gmail.com>
2025-03-03 08:41:15 +0800

    fix: fix the __init__ bug

--- src/zero_overhead_pinned_memory/__init__.py
+++ src/zero_overhead_pinned_memory/__init__.py
@@ -1,4 +1,3 @@
 from .cuda_mem_manager import to_posix_memalign_pinned, to_cuda_host_alloc_pinned, zeros_cuda_host_alloc_pinned
-from .patch import patch_deepspeed_zero_overhead_pinned_memory
 
-__all__ = ["to_posix_memalign_pinned", "to_cuda_host_alloc_pinned", "zeros_cuda_host_alloc_pinned", "patch_deepspeed_zero_overhead_pinned_memory"]
+__all__ = ["to_posix_memalign_pinned", "to_cuda_host_alloc_pinned", "zeros_cuda_host_alloc_pinned"]
